901c4afa7b69abc4d6f80263bd8b22a6c2daa662
3xtZzy <38046904+VanQube@users.noreply.github.com>
2021-03-11 02:37:35 +0300

    feat: add solution to task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let simpleBrackets = bracketsConfig.map( b => b.join("") );
+
+    for (let i = 0; i < simpleBrackets.length; i++) {
+
+        if( str.includes(simpleBrackets[i]) ) {
+            str = str.replace(simpleBrackets[i], "");
+            i = -1;
+        }
+    }
+    return  !str;
+
 }
